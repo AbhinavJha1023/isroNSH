95515c78c2cd358109ac84c8778302fbe967ad74
Abhinav R Jha <bt23csd053@iiitn.ac.in>
2024-08-25 14:47:55 +0530

    Create cell4.py

---
+++ cell4.py
@@ -0,0 +1,10 @@
+def get_model_bbox(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
